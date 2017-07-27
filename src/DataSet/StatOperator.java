a9e69ce708197b3db7ef8dfc6562ceff5f51a405
Emmanuel <emmanuel.salcido.maldonado@gmail.com>
2017-07-26 22:16:43 -0500

    StatOperator class added

---
+++ src/DataSet/StatOperator.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataSet;
+
+import java.util.*;
+import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
+
+/**
+ *
+ * @author Emmanuel
+ */
+public class StatOperator {
+    
+    private List<String> varData;
+    private SummaryStatistics summary;
+    
+    public StatOperator(List data){
+        this.varData = data;
+        if(varData != null){
+            for(String value : varData){
+                summary.addValue(Double.parseDouble(value));
+            }
+        }
+    }
+    
+    public double calMean(){
+        return summary.getMean();
+    }
+    
+}
