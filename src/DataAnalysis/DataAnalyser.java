e53db22a8de6a076ea0d5f2f95e45db930c54e22
Emmanuel <emmanuel.salcido.maldonado@gmail.com>
2017-12-05 23:44:57 -0600

    Update DataAnalyser.

--- src/DataAnalysis/DataAnalyser.java
+++ src/DataAnalysis/DataAnalyser.java
@@ -7,6 +7,7 @@ package DataAnalysis;
 
 import DataSet.DataDef;
 import DataSet.TableData;
+import DataSet.VariableType;
 import java.io.IOException;
 import java.util.Map;
 
@@ -27,15 +28,148 @@ public class DataAnalyser {
     public void startAnalyseTable(){
         
         for(Map.Entry<String, DataDef> entry : tableData.getDefMap().entrySet()){
-            
                 DataDef dataDef = entry.getValue();
-                outputResult.writeVariableSummary(dataDef);
+                writeVariableSummary(dataDef);
         }
         try{
             outputResult.closeFile();
         }
         catch(IOException iOE){
                 System.out.println("DataAnalyser | startAnalyseTable | IOException | " + iOE.getMessage());
+        }   
+    }
+    
+    private String analyseSymmetry(double skewness, double kurtosis){
+        String line = "";
+        if(skewness == 0)
+            line += "The data is symetric and with normal distribution. ";
+        else if(skewness < -0.5d)
+            line += "The data is not symetric and the left tail is long relative to right tail. ";
+        else if(skewness > 0.5d)
+            line += "The data is not symetric and the right tail is long relative to left tail. ";
+        if(kurtosis > 0.5d)
+            line += "Also the tails of the data are heavy or large";
+        else if(kurtosis < -0.5)
+            line += "Also the tails of the data are light or small";
+        return line;
+    }
+    
+    private String analyseSpread(double stDev, double smallDif){
+        String line = "";
+        if(stDev/2 - smallDif > 0)
+            line += "Data is spread and possible with outliers which impact in this.";
+        else if(stDev/2 - smallDif < 0)
+            line += "Data is not spread and should be accumulated";
+        return line;
+    }
+    
+    private void writeVariableSummary(DataDef data){
+        try{
+            outputResult.printSeparator();
+            outputResult.writeLine("Variable Name:");
+            outputResult.writeLine(" "+data.getName().toString());
+            outputResult.printBlankLine();
+            outputResult.writeLine("Variable Types: ");
+            for(VariableType type:data.getVarType())
+                outputResult.writeLine(" "+type.toString());
+            if(data.getMapedType() != null){
+                outputResult.writeLine("Variable maped to:");
+                outputResult.writeLine(" "+data.getMapedType().toString());
+            }
+            if(data.getVarSubType() != null){
+                outputResult.writeLine("Variable subtype:");
+                outputResult.writeLine(" "+data.getVarSubType().toString());
+            }
+            outputResult.writeLine("Mean | Sum of numbers divided by N");
+            outputResult.writeLine(" "+String.valueOf(data.getMean()));
+            outputResult.writeLine("Geometric Mean | Nth root of product of all numbers");
+            outputResult.writeLine(" "+String.valueOf(data.getGeometricMean()));
+            outputResult.writeLine("Mode | The most commonly reported value are");
+            outputResult.printDoubleArray(data.getMode());            
+            outputResult.writeLine("Median | Middle entry in sorted sequence");
+            outputResult.writeLine(" "+String.valueOf(data.getMedian()));
+            outputResult.writeLine("Range | Maximum subtracting Minimum");
+            outputResult.writeLine(" "+String.valueOf(data.getRange()));
+            outputResult.writeLine("Min value:");
+            outputResult.writeLine(" "+String.valueOf(data.getMin()));
+            outputResult.writeLine("Max value:");
+            outputResult.writeLine(" "+String.valueOf(data.getMax()));
+            outputResult.writeLine("Quartile 1 | Splits off the lowest 25% of data from the highest 75%");
+            outputResult.writeLine(" "+String.valueOf(data.getQ1()));
+            outputResult.writeLine("Quartile 3 | Splits off the highest 25% of data from the lowest 75%");
+            outputResult.writeLine(" "+String.valueOf(data.getQ3()));
+            outputResult.writeLine("Interquartile Range | Q3 - Q1 ");
+            outputResult.writeLine(" "+String.valueOf(data.getIqr()));
+            outputResult.writeLine("Low Interquartile Range:");
+            outputResult.writeLine(" "+String.valueOf(data.getLowIqr()));
+            outputResult.writeLine("High Interquartile Range:");
+            outputResult.writeLine(" "+String.valueOf(data.getHighIqr()));
+            outputResult.writeLine("Skewness:");
+            outputResult.writeLine(" "+String.valueOf(data.getSkewness()));
+            outputResult.writeLine("Kurtosis:");
+            outputResult.writeLine(" "+String.valueOf(data.getKurtosis()));
+            outputResult.writeLine(analyseSymmetry(data.getSkewness(),data.getKurtosis()));
+            outputResult.writeLine("Standard Deviation | Spread of the data from the mean");
+            outputResult.writeLine(" "+String.valueOf(data.getStandrDev()));
+            outputResult.writeLine(analyseSpread(data.getStandrDev(),data.getMinDif()));
+            outputResult.writeLine("Standar Error:");
+            outputResult.writeLine(" "+String.valueOf(data.getStandrErr()));
+            outputResult.writeLine("Low Quartile Not by lib:");
+            outputResult.writeLine(" "+String.valueOf(data.getLowQuartile()));
+            outputResult.writeLine("Mid Quartile Not by lib:");
+            outputResult.writeLine(" "+String.valueOf(data.getMidQuartile()));
+            outputResult.writeLine("Up Quartile Not by lib:");
+            outputResult.writeLine(" "+String.valueOf(data.getUpQuartile()));
+            outputResult.writeLine("Range Quartile:");
+            outputResult.writeLine(" "+String.valueOf(data.getRangeQuartile()));
+            outputResult.writeLine("Variance | Spread of data and measure of how much the values differ from mean");
+            outputResult.writeLine(" "+String.valueOf(data.getVariance()));
+            outputResult.writeLine("Pupulation Variance | Spread of data and measure of how much the values differ from mean");
+            outputResult.writeLine(" "+String.valueOf(data.getPopulationVariance()));
+            outputResult.writeLine("Coefficient Variation:");
+            outputResult.writeLine(" "+String.valueOf(data.getCoefficientVariation()));
+            outputResult.writeLine("Minimum difference between values:");
+            outputResult.writeLine(" "+String.valueOf(data.getMinDif()));
+            outputResult.writeLine("Population:");
+            outputResult.writeLine(" "+String.valueOf(data.getPopulation()));
+            outputResult.writeLine("Is this variable important to set?");
+            outputResult.writeLine(" "+String.valueOf(data.getIsEnable()));
+            outputResult.printBlankLine();
+            outputResult.printSubSeparator();
+            outputResult.writeLine("Normilize Data:");
+            outputResult.printDoubleArray(data.getNormilizeData());
+            outputResult.writeLine("Distinct values: ");
+            outputResult.printDistinctMap(data.getDistValues());
+            if(data.getIndexOutliersList() != null){
+                outputResult.writeLine("Index of possible outliers: ");
+                outputResult.printIntegerList(data.getIndexOutliersList());
+            }
+            if(data.getNumericValues() != null){
+                outputResult.writeLine("Numeric Values: ");
+                outputResult.printDoubleList(data.getNumericValues());
+            }
+            outputResult.writeLine("Original Values: ");
+            outputResult.printStringList(data.getOriginalValues());
+            if(data.getCategoricalValue() != null){
+                outputResult.writeLine("Categorical Values: ");
+                outputResult.printStringList(data.getCategoricalValue());
+            }
+            if(data.getRemapValues() != null){
+                outputResult.writeLine("Remap Values: ");
+                outputResult.printRemapMap(data.getRemapValues());
+            }
+            outputResult.printSubSeparator();
+        }
+        catch(IOException iOE){
+            System.out.println("DataAnalyser | IOException | File not found " + iOE.getMessage());
+        }
+        finally{
+            try{
+                outputResult.flushToFile();
+            }
+            catch(IOException iOE){
+                System.out.println("DataAnalyser | IOException | File not found " + iOE.getMessage());
+            }
         }
         
     }
--- src/DataAnalysis/OutputResult.java
+++ src/DataAnalysis/OutputResult.java
@@ -41,7 +41,7 @@ public class OutputResult {
     }
     
     public void writeLine(String line) throws IOException{
-        bufferedWriter.write(line);
+        bufferedWriter.write(" " + line);
         bufferedWriter.newLine();
     }
     
@@ -50,26 +50,26 @@ public class OutputResult {
         outputStream.close();
     }
     
-    private void flushToFile() throws IOException{
+    public void flushToFile() throws IOException{
         bufferedWriter.flush();
         outputStream.flush();
     }
     
-    private void printSeparator() throws IOException{
+    public void printSeparator() throws IOException{
         bufferedWriter.write("******************************************************************");
         bufferedWriter.newLine();
     }
     
-    private void printSubSeparator() throws IOException{
+    public void printSubSeparator() throws IOException{
         bufferedWriter.write("------------------------------");
         bufferedWriter.newLine();
     }
     
-    private void printBlankLine() throws IOException{
+    public void printBlankLine() throws IOException{
         bufferedWriter.newLine();
     }
     
-    private void printDistinctMap(Map<String,Integer> map) throws IOException{
+    public void printDistinctMap(Map<String,Integer> map) throws IOException{
         String line = "";
         for(Map.Entry<String, Integer> entry : map.entrySet()){
             line += "   |   " + entry.getKey() + " -> " + entry.getValue() + "  |   ";
@@ -77,37 +77,37 @@ public class OutputResult {
         writeLine(line);
     }
     
-    private void printRemapMap(Map<String,String> map) throws IOException{
+    public void printRemapMap(Map<String,String> map) throws IOException{
         for(Map.Entry<String, String> entry : map.entrySet()){
             writeLine(entry.getKey() + " - " + entry.getValue());
         }
     }
     
-    private void printStringList(List<String> list) throws IOException{
+    public void printStringList(List<String> list) throws IOException{
         String line = "";
         for(String value:list){
-            line = "    " + value.toString();
+            line += "    " + value.toString();
         }
         writeLine(line);
     }
     
-    private void printIntegerList(List<Integer> list) throws IOException{
+    public void printIntegerList(List<Integer> list) throws IOException{
         String line = "";
         for(Integer value:list){
-            line = "    " + value.toString();
+            line += "    " + value.toString();
         }
         writeLine(line);
     }
     
-    private void printDoubleList(List<Double> list) throws IOException{
+    public void printDoubleList(List<Double> list) throws IOException{
         String line = "";
         for(Double value:list){
-            line = "    " + value.toString();
+            line += "    " + value.toString();
         }
         writeLine(line);
     }
     
-    private void printDoubleArray(double[] array) throws IOException{
+    public void printDoubleArray(double[] array) throws IOException{
         String line = "";
         for(double value:array){
             line += "   " + value;
@@ -115,82 +115,4 @@ public class OutputResult {
         writeLine(line);
     }
     
-    public void writeVariableSummary(DataDef data){
-        try{
-            printSeparator();
-            writeLine("Variable Name: " + data.getName().toString());
-            printBlankLine();
-            writeLine("Variable Types: ");
-            for(VariableType type:data.getVarType())
-                writeLine(type.toString());
-            if(data.getMapedType() != null)
-                writeLine("Variable maped to: " + data.getMapedType().toString());
-            if(data.getVarSubType() != null)
-                writeLine("Variable subtype: " + data.getVarSubType().toString());
-            writeLine("Mean: " + data.getMean());
-            writeLine("Mode: ");
-            printDoubleArray(data.getMode());
-            writeLine("Geometric Mean:" + data.getGeometricMean());
-            writeLine("Median: " + data.getMedian());
-            writeLine("Range: " + data.getRange());
-            writeLine("Min value: " + data.getMin());
-            writeLine("Max value: " + data.getMax());
-            writeLine("Normilize Data:");
-            printDoubleArray(data.getNormilizeData());
-            writeLine("Quartile 1: " + data.getQ1());
-            writeLine("Quartile 3: " + data.getQ3());
-            writeLine("Interquartile Range: " + data.getIqr());
-            writeLine("Low Interquartile Range: " + data.getLowIqr());
-            writeLine("High Interquartile Range: " + data.getHighIqr());
-            writeLine("Skewness: " + data.getSkewness());
-            writeLine("Standar Deviation: " + data.getStandrDev());
-            writeLine("Standar Error: " + data.getStandrErr());
-            writeLine("Low Quartile Not by lib: " + data.getLowQuartile());
-            writeLine("Mid Quartile Not by lib: " + data.getMidQuartile());
-            writeLine("Up Quartile Not by lib: " + data.getUpQuartile());
-            writeLine("Range Quartile: " + data.getRangeQuartile());
-            writeLine("Variance: " + data.getVariance());
-            writeLine("Pupulation Variance: " + data.getPopulationVariance());
-            writeLine("Coefficient Variation: " + data.getCoefficientVariation());
-            writeLine("Kurtosis: " + data.getKurtosis());
-            writeLine("Minimum difference between values: " + data.getMinDif());
-            writeLine("Population: " + data.getPopulation());
-            writeLine("Is this variable important to set? " + data.getIsEnable());
-            printBlankLine();
-            printSubSeparator();
-            writeLine("Distinct values: ");
-            printDistinctMap(data.getDistValues());
-            if(data.getIndexOutliersList() != null){
-                writeLine("Index of possible outliers: ");
-                printIntegerList(data.getIndexOutliersList());
-            }
-            if(data.getNumericValues() != null){
-                writeLine("Numeric Values: ");
-                printDoubleList(data.getNumericValues());
-            }
-            writeLine("Original Values: ");
-            printStringList(data.getOriginalValues());
-            if(data.getCategoricalValue() != null){
-                writeLine("Categorical Values: ");
-                printStringList(data.getCategoricalValue());
-            }
-            if(data.getRemapValues() != null){
-                writeLine("Remap Values: ");
-                printRemapMap(data.getRemapValues());
-            }
-            printSubSeparator();
-        }
-        catch(IOException iOE){
-            System.out.println("DataAnalyser | IOException | File not found " + iOE.getMessage());
-        }
-        finally{
-            try{
-                flushToFile();
-            }
-            catch(IOException iOE){
-                System.out.println("DataAnalyser | IOException | File not found " + iOE.getMessage());
-            }
-        }
-        
-    }
 }
